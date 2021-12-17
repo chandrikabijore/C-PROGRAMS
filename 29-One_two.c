8913a1d9f593d752011c252ddc0c6de94c341637
chandrika bijore <chandrikabijore@gmail.com>
2021-12-17 18:49:41 +0530

    function

---
+++ 29-One_two.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+void main()
+{
+    int a;
+    printf("Enter any number : ");
+    scanf("%d", &a);
+
+    if (a == 1)
+    {
+        printf("one");
+    }
+
+    else if (a == 2)
+    {
+        printf("Two");
+    }
+
+    else if (a == 3)
+    {
+        printf("Three");
+    }
+
+    else if (a == 4)
+    {
+        printf("four");
+    }
+
+    else if (a == 5)
+    {
+        printf("five");
+    }
+
+    else if (a == 6)
+    {
+        printf("six");
+    }
+
+    else if (a == 7)
+    {
+        printf("seven");
+    }
+    else if (a == 8)
+    {
+        printf("seven");
+    }
+    else if (a == 9)
+    {
+        printf("seven");
+    }
+    else if (a == 10)
+    {
+        printf("seven");
+    }
+    else
+    {
+        printf("this is default");
+    }
+}
+
